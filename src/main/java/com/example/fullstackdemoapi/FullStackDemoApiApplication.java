be4ccadd86c940d59a14b4a1a01c0cd1420088a2
Dante Garcia <dag4799@yahoo.com>
2022-08-15 17:33:01 -0500

    initial commit

---
+++ src/main/java/com/example/fullstackdemoapi/FullStackDemoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullstackdemoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackDemoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackDemoApiApplication.class, args);
+    }
+
+}
